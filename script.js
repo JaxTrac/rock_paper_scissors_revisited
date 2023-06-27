b2447bfd859c2ed376fc9dfa997ccc6a3a599599
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-06-27 17:46:36 +0200

    Refine getComputerChoice function to words

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        return "rock";
+    } else if (computerChoice == 1) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
