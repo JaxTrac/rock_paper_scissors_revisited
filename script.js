490a8df38382207dee57a0ddfc7de3362c33fc3f
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-06-27 18:11:15 +0200

    Create playRound function

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLowerCase = playerSelection.toLowerCase();
+    if (playerSelectionLowerCase == computerSelection) {
+        return "Draw!"
+    } else if (playerSelectionLowerCase == "rock" && computerSelection == "scissors"){
+        return "You win!"
+    } else if (playerSelectionLowerCase == "paper" && computerSelection == "rock"){
+        return "You win!"
+    }else if (playerSelectionLowerCase == "scissors" && computerSelection == "paper"){
+        return "You win!"
+    }else {
+        return "You looose!"
+    }
+  }
+   
+
+let test = getComputerChoice()
+
+console.log(playRound("SCISSORS", test));
+console.log(playRound("Rock", test));
+console.log(playRound("paper", test));
